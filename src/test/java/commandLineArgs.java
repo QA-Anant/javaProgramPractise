4f4f6bb140e85c3134e359c390c2eebdf2e86012
anant.jain <anant.jain@SDI-HLANANTJ.local>
2024-01-15 19:38:40 +0530

    test

--- src/test/java/commandLineArgs.java
+++ src/test/java/commandLineArgs.java
@@ -1,6 +1,16 @@
+import static java.lang.Integer.parseInt;
+import static java.lang.System.*;
+
 public class commandLineArgs {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Number of arguments passed: "+args.length);
+        out.println("Hello World");
+
+        int a, b,c;
+        a= parseInt(args[0]);
+        b= parseInt(args[1]);
+        c= a+b;
+
+        out.println("Number of arguments passed: "+args.length);
+        out.println("Sum of two numbers: "+c);
     }
 }
