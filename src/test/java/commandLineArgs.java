f86e6728f6640fc56c30bfbac287fe0a210217cd
anant.jain <anant.jain@SDI-HLANANTJ.local>
2024-01-15 18:25:42 +0530

    test push

--- src/test/java/commandLineArgs.java
+++ src/test/java/commandLineArgs.java
@@ -1,5 +1,6 @@
 public class commandLineArgs {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Number of arguments passed: "+args.length);
     }
 }
